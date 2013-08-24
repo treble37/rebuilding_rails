9ee6218c159aafb7ee70b6f5fcd34a267f0933fa
BruceP <brucepark0@gmail.com>
2013-08-23 17:15:51 -0700

    chapter 2

---
+++ best_quotes/app/controllers/quotes_controller.rb
@@ -0,0 +1,6 @@
+# best_quotes/app/controllers/quotes_controller.rb
+class QuotesController < Rulers::Controller
+  def a_quote
+    "There is nothing either good or bad " + "but thinking makes it so."
+  end
+end
