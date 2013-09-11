ed086554d35c745fb73c962667afcc9b4f305d4c
BruceP <brucepark0@gmail.com>
2013-09-11 11:27:07 -0700

    modified views

--- best_quotes/app/controllers/quotes_controller.rb
+++ best_quotes/app/controllers/quotes_controller.rb
@@ -1,7 +1,7 @@
 # best_quotes/app/controllers/quotes_controller.rb
 class QuotesController < Rulers::Controller
   def a_quote
-    "There is nothing either good or bad " + "but thinking makes it so." + "\n<pre>\n#{env}\n</pre>"
+    render :a_quote, :noun => :winking
   end
   def redirected_quote
     "Redirecting quotes..."
