f838378b00dc6f9b7e5d2bbd2c91c3bec3dfcd66
BruceP <brucepark0@gmail.com>
2013-09-17 17:11:52 -0700

    added new methods

--- best_quotes/app/controllers/quotes_controller.rb
+++ best_quotes/app/controllers/quotes_controller.rb
@@ -9,4 +9,21 @@ class QuotesController < Rulers::Controller
   def exception
     raise "It's a bad one!"
   end
+  def quote_1
+    quote_1 = FileModel.find(1)
+    render :quote, :obj => quote_1
+  end
+  def index
+    quotes = FileModel.all
+    render :index, :quotes => quotes
+  end
+  def new_quote
+    attrs = {
+      "submitter" => "web user",
+      "quote" => "A picture is worth a thousand pixels.",
+      "attribution" => "Me"
+    }
+    m = FileModel.create attrs
+    render :quote, :obj => m
+  end
 end
